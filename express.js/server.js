58e24edadc78522ad00c799e80b656be0653522a
Preston20050813 <80573207+Preston20050813@users.noreply.github.com>
2024-08-12 11:14:11 +0800

    feat. singleton implementation for knex

--- express.js/server.js
+++ express.js/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const MysqlConn = require("./mysql_conn");
 const app = express();
 
 port = 3000;
 app.listen(3000);
+
+const mysqlConn = MysqlConn.getInstance();
+const knex = mysqlConn.getDB;
