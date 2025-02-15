20fc138e9f3cec8ec04bb0065576e0ad61c7dea5
Alex Zhang <alexzhang0930@gmail.com>
2025-02-15 14:04:12 -0500

    updated search bar

--- backend/src/main_page/SearchBar/SearchBar.jsx
+++ backend/src/main_page/SearchBar/SearchBar.jsx
@@ -1,38 +1,58 @@
-import React, {useState} from "react";
-import {FaSearch} from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css";
 
-export const SearchBar = ({setResults}) => {
-
-    const [input, setInput] = useState("")
+export const SearchBar = ({ setResults }) => {
+    const [input, setInput] = useState("");
+    const [tempInput, setTempInput] = useState("");
 
     const fetchData = (value) => {
-        //this is temporary just to use as an example
         fetch("https://jsonplaceholder.typicode.com/users")
             .then((response) => response.json())
             .then((json) => {
-                const results = json.filter((user) => {
-                    return (
-                        value && 
-                        user && 
-                        user.name && 
-                        user.name.toLowerCase().includes(value.toLowerCase()));
-                });
+                let results;
+                if (value) {
+                    results = json.filter((user) => {
+                        return (
+                            user &&
+                            user.name &&
+                            user.name.toLowerCase().includes(value.toLowerCase())
+                        );
+                    });
+                } else {
+                    results = json;
+                }
                 setResults(results);
             });
-    }
+    };
 
-    const handleChange = (value) => {
-        setInput(value)
-        fetchData(value)
-    }
+    useEffect(() => {
+        // Fetch data initially without any filtering
+        fetchData("");
+    }, []);
 
-    return <div className="input-wrapper">
-        <FaSearch id="search-icon" />
-        <input 
-            placeHolder="Type to search..." 
-            value={input} 
-            onChange={(e) => handleChange(e.target.value)}
-        />
-    </div>
-}
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            handleSubmit(tempInput);
+        }
+    };
+
+    const handleSubmit = (value) => {
+        setInput(value);
+        fetchData(value);
+    };
+
+    return (
+        <div className="input-wrapper">
+            <FaSearch id="search-icon" />
+            <input
+                type="text"
+                placeholder="Type to search..."
+                value={tempInput}
+                onChange={(e) => setTempInput(e.target.value)}
+                onKeyDown={(e) => handleKeyDown(e)}
+                autoComplete="off"
+            />
+        </div>
+    );
+};
