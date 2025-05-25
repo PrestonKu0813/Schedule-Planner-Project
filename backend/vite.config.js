cc0cda2d40781a112f3c50e60ed69a02174a223d
Preston20050813 <80573207+Preston20050813@users.noreply.github.com>
2025-05-25 12:01:49 -0400

    build. added proxy for vite

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const serverBaseURL = "http://localhost:300";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/auth": serverBaseURL,
+      "/profile": serverBaseURL,
+      "/explore": serverBaseURL,
+      "/course": serverBaseURL,
+    },
+  },
+});
