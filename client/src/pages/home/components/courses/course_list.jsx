688ba8154c0eae44d3af7aae2154de57f232cb3b
Preston20050813 <80573207+Preston20050813@users.noreply.github.com>
2025-06-05 16:11:16 -0400

    style. update course list position

--- client/src/pages/home/components/courses/course_list.jsx
+++ client/src/pages/home/components/courses/course_list.jsx
@@ -1,12 +1,22 @@
 import "./course_list.css";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { SearchResultsList } from "../SearchBar/SearchResultsList";
-import { useState, useEffect } from 'react';
-import Select from 'react-select';
+import { useState, useEffect } from "react";
+import Select from "react-select";
 
-function CourseList({courses, setCourses, info, setInfo, activeTab, setActiveTab}) {
+function CourseList({
+  courses,
+  setCourses,
+  info,
+  setInfo,
+  activeTab,
+  setActiveTab,
+}) {
   const [results, setResults] = useState([]);
-  const [selectedTag, setSelectedTag] = useState({ value: "all", label: "All" });
+  const [selectedTag, setSelectedTag] = useState({
+    value: "all",
+    label: "All",
+  });
 
   const tagOptions = [
     { value: "all", label: "All" },
@@ -21,35 +31,39 @@ function CourseList({courses, setCourses, info, setInfo, activeTab, setActiveTab
   //   handleTabChange(activeTab);
   // }, [activeTab]);
 
-
-
   return (
-    <div>
-      <button
-        className={`course_list_explore_button ${
-          activeTab === "EXPLORE" ? "active" : ""
-        }`}
-        onClick={() => setActiveTab("EXPLORE")}
-      >
-        EXPLORE
-      </button>
+    <div className="course_list_inner">
+      <div className="button_row">
+        <button
+          className={`course_list_explore_button ${
+            activeTab === "EXPLORE" ? "active" : ""
+          }`}
+          onClick={() => setActiveTab("EXPLORE")}
+        >
+          EXPLORE
+        </button>
 
-      <button
-        className={`course_list_courses_button ${
-          activeTab === "COURSES" ? "active" : ""
-        }`}
-        onClick={() => setActiveTab("COURSES")}
-      >
-        COURSES
-      </button>
-
+        <button
+          className={`course_list_courses_button ${
+            activeTab === "COURSES" ? "active" : ""
+          }`}
+          onClick={() => setActiveTab("COURSES")}
+        >
+          COURSES
+        </button>
+      </div>
       {/* Tab Content */}
-      <div className={`course_list ${activeTab === "EXPLORE" ? "explore-bg" : "courses-bg"}`}>
+      <div
+        className={`course_list ${activeTab === "EXPLORE" ? "explore-bg" : "courses-bg"}`}
+      >
         {activeTab === "EXPLORE" ? (
           <div className="course_list_text">
             <h2>Explore Tab</h2>
             <div className="search-bar-container">
-              <SearchBar setResults={setResults} selectedTag={selectedTag.value} />
+              <SearchBar
+                setResults={setResults}
+                selectedTag={selectedTag.value}
+              />
               <div className="subject-filter">
                 <label htmlFor="userId">Filter by Subject:</label>
                 <Select
@@ -59,7 +73,13 @@ function CourseList({courses, setCourses, info, setInfo, activeTab, setActiveTab
                   onChange={setSelectedTag}
                 />
               </div>
-              <SearchResultsList results={results} courses={courses} setCourses={setCourses} setInfo={setInfo} setActiveTab={setActiveTab}/>
+              <SearchResultsList
+                results={results}
+                courses={courses}
+                setCourses={setCourses}
+                setInfo={setInfo}
+                setActiveTab={setActiveTab}
+              />
             </div>
           </div>
         ) : (
