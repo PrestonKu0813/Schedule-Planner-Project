61e7fdce3fd1a0726036c99243ca73973c1b7df7
Preston20050813 <80573207+Preston20050813@users.noreply.github.com>
2025-06-15 15:15:34 -0400

    feat. added layout structure for login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Login from "./pages/Login";
+import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         {/* <Route path="*" element={<Login />} /> */}
       </Routes>
---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,13 @@
+import "./login_layout.css";
+import React, { useState } from "react";
+
+export default function Login() {
+  return (
+    <div className="layout_container">
+      <div className="side_binder">
+        <div className="second_side_binder"></div>
+      </div>
+      <div className="login_content"></div>
+    </div>
+  );
+}
