98200a940ec4a4446f0b4484050138b5f70e5ca1
Stevuie <sh1746@scarletmail.rutgers.edu>
2025-06-17 21:47:40 -0400

    style. adding center div for title text

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -3,11 +3,18 @@ import React, { useState } from "react";
 
 export default function Login() {
   return (
-    <div className="layout_container">
+    <div className="login_layout_container">
       <div className="side_binder">
         <div className="second_side_binder"></div>
       </div>
-      <div className="login_content"></div>
+      <div className="login_content">
+        <div className="title_container">
+          <div className="title_rows">
+          </div>
+          <div className="title_rows">
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
